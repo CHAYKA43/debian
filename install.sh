cae77e5644587d0e54a864831f9ddcf113abb023
CHAYKA43 <makar.lososev@gmail.com>
2025-04-13 21:45:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "🔑 Импортируем GPG ключ..."
 curl -fsSL https://chayka43.github.io/debian/public_key.asc | sudo tee /etc/apt/trusted.gpg.d/chayka43-debian.asc > /dev/null
 
 echo "📦 Добавляем репозиторий..."
-echo "deb https://chayka43.github.io/debian stable main" | sudo tee /etc/apt/sources.list.d/chayka43-debian.list
+echo "deb https://chayka43.github.io/debian stable main" | sudo tee /etc/apt/sources.list.d/chayka43-debian.list > /dev/null
 
 echo "🌀 Обновляем индексы пакетов..."
 sudo apt update
